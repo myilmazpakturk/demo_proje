37dfd170d022d07229719299ab6aed7ff3e33e17
Mustafa <myilmazpakturk@gmail.com>
2020-06-20 16:16:37 -0400

    ikinci surum repodan

--- src/demo_proje/DemoClass.java
+++ src/demo_proje/DemoClass.java
@@ -5,6 +5,7 @@ public class DemoClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit local repo dan");
 		System.out.println("ilk commit from localdan");
+		System.out.println("ikinci commit from repo dan");
 	}
 
 }
